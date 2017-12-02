886b8b90104135519a8b4eac2fe173f8fc441c3f
Karolina <31165670+CharlotteMoriarty@users.noreply.github.com>
2017-12-02 21:51:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+$(function(){
+    var url = 'https://restcountries.eu/rest/v1/name/';
+
+    var countriesList = $('#countries');
+    $('#search').click(searchCountries);
+    function searchCountries() {
+        var countryName = $('#country-name').val();
+        if(!countryName.length) countryName = 'Poland';
+        $.ajax({
+            url: url + countryName,
+            method: 'GET',
+            success: showCountriesList
+        });
+    }
+   
+    function showCountriesList(resp) {
+        countriesList.empty();
+        resp.forEach(function(item){
+           
+            $('#countries').text(item.name).appendTo(countriesList);
+            //$('<li id="flag">').text(item.flag).appendTo(countriesList);     
+            $('<li id="capital">').text(item.capital).appendTo(countriesList);
+            $('<li id="languages">').text(item.languages).appendTo(countriesList);
+            $('<li id="borders">').text(item.borders).appendTo(countriesList);
+            $('<li id="currencies">').text(item.currencies).appendTo(countriesList);
+            $('<li id="latlng">').text(item.latlng).appendTo(countriesList);
+        });
+    };
+    
+});
